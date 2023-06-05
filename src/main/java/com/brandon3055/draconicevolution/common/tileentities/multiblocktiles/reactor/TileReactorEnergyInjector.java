72b65b8c12ff762f8e3571b0fd666056ec8b525f
Alexander Anishin <oneeyemaker@gmail.com>
2023-06-05 20:03:49 +0300

    Optimized Reactor Energy Injector

--- src/main/java/com/brandon3055/draconicevolution/common/tileentities/multiblocktiles/reactor/TileReactorEnergyInjector.java
+++ src/main/java/com/brandon3055/draconicevolution/common/tileentities/multiblocktiles/reactor/TileReactorEnergyInjector.java
@@ -1,8 +1,5 @@
 package com.brandon3055.draconicevolution.common.tileentities.multiblocktiles.reactor;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
@@ -12,11 +9,10 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.util.ForgeDirection;
 
-import com.brandon3055.brandonscore.common.utills.Utills;
 import com.brandon3055.draconicevolution.DraconicEvolution;
 import com.brandon3055.draconicevolution.client.render.particle.ParticleReactorBeam;
 import com.brandon3055.draconicevolution.common.blocks.multiblock.IReactorPart;
-import com.brandon3055.draconicevolution.common.blocks.multiblock.MultiblockHelper;
+import com.brandon3055.draconicevolution.common.blocks.multiblock.MultiblockHelper.TileLocation;
 import com.brandon3055.draconicevolution.integration.computers.IDEPeripheral;
 
 import cofh.api.energy.IEnergyReceiver;
@@ -30,30 +26,32 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
 
     public float modelIllumination = 1F;
     public int facingDirection = ForgeDirection.UP.ordinal();
-    public MultiblockHelper.TileLocation masterLocation = new MultiblockHelper.TileLocation();
+    public TileLocation masterLocation = new TileLocation();
     public boolean isValid = false;
-    public int tick = 0;
 
     @SideOnly(Side.CLIENT)
     private ParticleReactorBeam beam;
 
-    private TileReactorCore core = null;
-    private int redstoneMode = 0;
-    private int rs = -1;
-    private int rsCach = -1;
+    private int redstoneMode = IReactorPart.RMODE_TEMP;
+    private int comparatorOutputCache = -1;
 
     @Override
     public void updateEntity() {
         if (worldObj.isRemote && isValid) {
             updateBeam();
+            return;
         }
 
-        TileEntity master = masterLocation.getTileEntity(worldObj);
-        if (!worldObj.isRemote && master instanceof TileReactorCore) {
-            rs = ((TileReactorCore) master).getComparatorOutput(redstoneMode);
-            if (rs != rsCach) {
-                rsCach = rs;
-                Utills.updateNeabourBlocks(worldObj, xCoord, yCoord, zCoord);
+        TileReactorCore core = getMaster();
+        if (core != null) {
+            int comparatorOutput = core.getComparatorOutput(redstoneMode);
+            if (comparatorOutput != comparatorOutputCache) {
+                comparatorOutputCache = comparatorOutput;
+                worldObj.notifyBlocksOfNeighborChange(
+                        xCoord,
+                        yCoord,
+                        zCoord,
+                        worldObj.getBlock(xCoord, yCoord, zCoord));
             }
         }
     }
@@ -64,30 +62,43 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
     }
 
     public void onPlaced() {
-        checkForMaster();
-    }
-
-    public boolean checkForMaster() {
-        for (int i = 1; i < 10; i++) {
-            ForgeDirection dir = ForgeDirection.getOrientation(facingDirection);
-            int x = xCoord + (dir.offsetX * i);
-            int y = yCoord + (dir.offsetY * i);
-            int z = zCoord + (dir.offsetZ * i);
-            if (!worldObj.isAirBlock(x, y, z)) {
-                TileEntity tile = worldObj.getTileEntity(x, y, z);
-                if (tile instanceof TileReactorCore) {
-                    masterLocation.set(x, y, z);
-                    isValid = true;
-                    worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
-                    return true;
-                } else {
-                    isValid = false;
-                    return false;
-                }
+        ForgeDirection facing = ForgeDirection.getOrientation(facingDirection);
+        for (int distance = 1; distance <= TileReactorCore.MAX_SLAVE_RANGE; distance++) {
+            int targetX = xCoord + facing.offsetX * distance;
+            int targetY = yCoord + facing.offsetY * distance;
+            int targetZ = zCoord + facing.offsetZ * distance;
+            TileEntity tile = worldObj.getTileEntity(targetX, targetY, targetZ);
+            if (tile instanceof TileReactorCore) {
+                setUp(new TileLocation(targetX, targetY, targetZ));
+                return;
             }
         }
+        shutDown();
+    }
+
+    @Override
+    public TileLocation getMasterLocation() {
+        return masterLocation;
+    }
+
+    @Override
+    public TileReactorCore getMaster() {
+        TileEntity tile = masterLocation.getTileEntity(worldObj);
+        return tile instanceof TileReactorCore ? (TileReactorCore) tile : null;
+    }
+
+    @Override
+    public void setUp(TileLocation masterLocation) {
+        this.masterLocation = masterLocation;
+        isValid = true;
+        worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+    }
+
+    @Override
+    public void shutDown() {
+        masterLocation = new TileLocation();
         isValid = false;
-        return false;
+        worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
     }
 
     @Override
@@ -96,14 +107,8 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
     }
 
     @Override
-    public String getRedstoneModeString() {
-        return StatCollector.translateToLocal("msg.de.reactorRSMode." + redstoneMode + ".txt");
-    }
-
-    @Override
-    public void changeRedstoneMode() {
-        if (redstoneMode == IReactorPart.RMODE_FUEL_INV) redstoneMode = 0;
-        else redstoneMode++;
+    public ForgeDirection getFacing() {
+        return ForgeDirection.getOrientation(facingDirection);
     }
 
     @Override
@@ -112,10 +117,17 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
     }
 
     @Override
-    public void shutDown() {
-        masterLocation = new MultiblockHelper.TileLocation();
-        isValid = false;
-        worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+    public void changeRedstoneMode() {
+        if (redstoneMode == IReactorPart.RMODE_FUEL_INV) {
+            redstoneMode = IReactorPart.RMODE_TEMP;
+        } else {
+            redstoneMode++;
+        }
+    }
+
+    @Override
+    public String getRedstoneModeAsString() {
+        return StatCollector.translateToLocal("msg.de.reactorRSMode." + redstoneMode + ".txt");
     }
 
     @Override
@@ -134,15 +146,6 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
         facingDirection = compound.getInteger("Facing");
         isValid = compound.getBoolean("IsValid");
         super.onDataPacket(net, pkt);
-    }
-
-    public TileReactorCore getCore() {
-        if (core == null || core.isInvalid()) {
-            if (getMaster().getTileEntity(worldObj) instanceof TileReactorCore)
-                core = (TileReactorCore) getMaster().getTileEntity(worldObj);
-            else core = null;
-        }
-        return core;
     }
 
     @Override
@@ -164,19 +167,19 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
     }
 
     @Override
-    public MultiblockHelper.TileLocation getMaster() {
-        return masterLocation;
-    }
-
-    @Override
     public AxisAlignedBB getRenderBoundingBox() {
         return AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, xCoord + 1, yCoord + 1, zCoord + 1);
     }
 
     @Override
     public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate) {
-        if (simulate) return Integer.MAX_VALUE;
-        else if (getCore() != null) return getCore().injectEnergy(maxReceive);
+        if (simulate) {
+            return Integer.MAX_VALUE;
+        }
+        TileReactorCore core = getMaster();
+        if (core != null) {
+            return core.injectEnergy(maxReceive);
+        }
         return 0;
     }
 
@@ -206,48 +209,8 @@ public class TileReactorEnergyInjector extends TileEntity implements IReactorPar
     }
 
     @Override
-    public Object[] callMethod(String method, Object... args) {
-        if (!(getMaster().getTileEntity(worldObj) instanceof TileReactorCore)) return null;
-        TileReactorCore reactor = (TileReactorCore) getMaster().getTileEntity(worldObj);
-
-        if (args.length > 0) throw new IllegalArgumentException("This method dose not accept arguments");
-
-        if (method.equals("getReactorInfo")) {
-            Map<Object, Object> map = new HashMap<Object, Object>();
-            map.put("temperature", Utills.round(reactor.reactionTemperature, 100));
-            map.put("fieldStrength", Utills.round(reactor.fieldCharge, 100));
-            map.put("maxFieldStrength", Utills.round(reactor.maxFieldCharge, 100));
-            map.put("energySaturation", reactor.energySaturation);
-            map.put("maxEnergySaturation", reactor.maxEnergySaturation);
-            map.put("fuelConversion", Utills.round((double) reactor.convertedFuel + reactor.conversionUnit, 1000));
-            map.put("maxFuelConversion", reactor.reactorFuel + reactor.convertedFuel);
-            map.put("generationRate", (int) reactor.generationRate);
-            map.put("fieldDrainRate", reactor.fieldDrain);
-            map.put("fuelConversionRate", (int) Math.round(reactor.fuelUseRate * 1000000D));
-            map.put(
-                    "status",
-                    reactor.reactorState == 0 ? "offline"
-                            : reactor.reactorState == 1 && !reactor.canStart() ? "charging"
-                                    : reactor.reactorState == 1 && reactor.canStart() ? "charged"
-                                            : reactor.reactorState == 2 ? "online"
-                                                    : reactor.reactorState == 3 ? "stopping" : "invalid");
-            return new Object[] { map };
-        } else if (method.equals("chargeReactor")) {
-            if (reactor.canCharge()) {
-                reactor.reactorState = TileReactorCore.STATE_START;
-                return new Object[] { true };
-            } else return new Object[] { false };
-        } else if (method.equals("activateReactor")) {
-            if (reactor.canStart()) {
-                reactor.reactorState = TileReactorCore.STATE_ONLINE;
-                return new Object[] { true };
-            } else return new Object[] { false };
-        } else if (method.equals("stopReactor")) {
-            if (reactor.canStop()) {
-                reactor.reactorState = TileReactorCore.STATE_STOP;
-                return new Object[] { true };
-            } else return new Object[] { false };
-        }
-        return new Object[] {};
+    public Object[] callMethod(String methodName, Object... args) {
+        TileReactorCore core = getMaster();
+        return core != null ? core.callMethod(methodName, args) : null;
     }
 }
